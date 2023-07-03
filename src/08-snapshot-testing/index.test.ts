ec31dad5e61f25dae9929ff8da57cc44956a0e8e
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-07-04 00:25:40 +0300

    feat: add 08-snapshot-testing partial solution

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,10 +1,16 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const list = {
+      value: 1,
+      next: { value: 2, next: { value: 3, next: { value: null, next: null } } },
+    };
+    const elementsList = [1, 2, 3];
+    const generatedList = generateLinkedList(elementsList);
+    expect(generatedList).toStrictEqual(list);
   });
 
   // Check match by comparison with snapshot
